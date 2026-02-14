e6d500f10f7f4686c9b3269468dc5b56640c6ea3
softworldqjin <softworldqjin@gmail.com>
2026-02-14 11:34:20 +0900

    Stack

--- src/chap04/IntStack.java
+++ src/chap04/IntStack.java
@@ -16,7 +16,12 @@ public class IntStack {
 
         }
     }
-
+    public int peek() throws EmptyIntStackException{
+        if (ptr <= 0) {
+            throw new EmptyIntStackException();
+        }
+        return stk[ptr - 1];
+    }
     public IntStack(int maxLen) {
         ptr = 0;
         capacity = maxLen;
@@ -31,7 +36,7 @@ public class IntStack {
         if (ptr >= capacity) {
             throw new OverflowIntStackException();
         }
-        return stk[ptr++];
+        return stk[ptr++] = x;
     }
 
     public int pop() throws EmptyIntStackException{
@@ -39,5 +44,45 @@ public class IntStack {
             throw new EmptyIntStackException();
         }
         return stk[--ptr];
+    }
+
+    public void clear() {
+        ptr = 0;
+    }
+
+    public int indexOf(int x) {
+        for (int i = ptr - 1; i >= 0; i--) {
+            if (stk[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int size() {
+        return ptr;
+    }
+
+    public boolean isEmpty() {
+        return ptr <= 0;
+    }
+
+    public boolean isFull() {
+        return ptr == capacity;
+    }
+
+    public void dump() {
+        if (ptr <= 0) {
+            System.out.println("스택이 비어있습니다");
+        } else {
+            for (int i = 0; i < ptr; i++) {
+                System.out.printf("%d ", stk[i]);
+            }
+            System.out.println();
+        }
     }
 }
