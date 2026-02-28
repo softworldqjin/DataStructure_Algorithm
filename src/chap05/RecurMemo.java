1f4dddc730c5cb1376f6efe038633d9b81eab224
softworldqjin <softworldqjin@gmail.com>
2026-02-28 14:32:32 +0900

    -

---
+++ src/chap05/RecurMemo.java
@@ -0,0 +1,32 @@
+package chap05;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RecurMemo {
+    static String[] memo;
+    static void recur(int n) {
+        if (memo[n + 1] != null) {
+            System.out.println(memo[n + 1]);
+        } else {
+            if (n > 0) {
+                recur(n - 1);
+                System.out.println(n);
+                recur(n - 2);
+                memo[n + 1] = memo[n] + n + "\n" + memo[n - 1];
+            } else {
+                memo[n + 1] = "";
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수를 입력하세요: ");
+        int x = sc.nextInt();
+
+        memo = new String[x + 2];
+        recur(x);
+
+    }
+}
